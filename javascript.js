f09d8adb476910dcd49d543b523b2ebba761e6f5
Spencer Ang <boomerandzapper@gmail.com>
2017-09-03 21:09:03 -0700

    Created Functionality of Naming Players

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 console.log("Starting...")
 
-console.log("Prompt:")
+console.log("Enter number of Players:")
 
 let input = process.openStdin();
-let players;
+let data = {};
+let progress = 0;
 let state = "Setup Players";
 
 input.addListener("data", function(input) {
@@ -15,14 +16,30 @@ input.addListener("data", function(input) {
       case "Setup Players":
         if (input_clean % 1 == 0 && input_clean > 1 && input_clean < 11) {
           console.log((`There will be: [${input}] players in the poker game.`).replace(/(\r\n|\n|\r)/gm,""))
-          players = input_clean;
+          data.players = input_clean;
+          data.names = [];
           state = "Player Names"
+          console.log(`Enter name for player ${progress + 1}.`);
         } else {
           console.log("Invalid Number of Players");
       }
       break;
 
-      case "Player Names":
+      case "Player Names": {
+        data.names[progress] = input_clean;
+        progress += 1;
+        if (data.players == progress) {
+          state = "Settings";
+         console.log("Players:")
+         data.names.forEach(function(name, index) {
+            console.log(`Player ${(index+1) + ": " + name}`);
+        })
+         console.log("Please Set Initial Funds")
+        }  else {
+          console.log(`Enter name for player ${progress + 1}.`);
+        }
+      }
+      break;
 
       default:
         console.log("ERROR")
